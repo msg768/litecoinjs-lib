7fe4dc3722373267047f62f476e3f2205529a05a
IrynaBohatchenko <IrynaBohatchenko@users.noreply.github.com>
2018-01-29 10:05:38 +0200

    Litecoin testnet added

--- src/networks.js
+++ src/networks.js
@@ -33,5 +33,15 @@ module.exports = {
     pubKeyHash: 0x30,
     scriptHash: 0x32,
     wif: 0xb0
+  },
+  litecoinTestnet: {
+    messagePrefix: '\x19Litecoin Signed Message:\n',
+    bip32: {
+      public: 0x0436EF7D,
+      private: 0x0436F6E1
+    },
+    pubKeyHash: 0x6F,
+    scriptHash: 0xc4,
+    wif: 0xef
   }
 }
